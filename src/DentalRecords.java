676d3e007073c2b19026deb9dd30ba52c7509df8
Lorenzo <rendlorenzo@gmail.com>
2022-10-24 09:07:28 -0400

    Final program for submission

--- src/DentalRecords.java
+++ src/DentalRecords.java
@@ -1,15 +1,14 @@
 import java.util.Scanner;
 public class DentalRecords {
     private static final Scanner keyboard = new Scanner (System.in);
+    private static final int MAX_TEETH = 8;
 
-    public static double roots(int A, int B, int C) {
-
-    }
     public static void main(String[] args) {
 
         // variable declaration
         int numFamMembers, i, j, k;
         String tooths;
+        char menuOption;
 
         //input
         System.out.println("Welcome to the Floridian Tooth Records");
@@ -23,12 +22,11 @@ public class DentalRecords {
         }
 
         String [] famNames = new String[numFamMembers];
-        char [][][] teeth = new char [numFamMembers][2][8];
+        char [][][] teeth = new char [numFamMembers][2][];
         for (i = 0; i < numFamMembers; i++){
             System.out.printf("Please enter the name of Family member %d \t: ", (i+1));
             famNames[i] = keyboard.next();
             System.out.println();
-
             //uppers
                 System.out.printf("Please enter the uppers for %s \t: ", famNames[i]);
                 tooths = keyboard.next();
@@ -40,15 +38,11 @@ public class DentalRecords {
                         tooths = tooths.toUpperCase();
                     }
                 }
-
                 while (tooths.length() > 8) {
                     System.out.print("Too many teeth, try again\t: ");
                     tooths = keyboard.next();
                 }
-
-                for (k = 0; k < tooths.length(); k++) {
-                    teeth [i][0][k] = tooths.charAt(k);
-                }
+                teeth [i][0] = tooths.toCharArray();
 
             // lowers
                 System.out.printf("Please enter the lowers for %s \t: ", famNames[i]);
@@ -66,64 +60,170 @@ public class DentalRecords {
                     System.out.print("Too many teeth, try again\t: ");
                     tooths = keyboard.next();
                 }
-
-                for (k = 0; k < tooths.length(); k++) {
-                    teeth [i][1][k] = tooths.charAt(k);
-                }
-
-        }
-        String menuOption;
-        System.out.print("(P)rint, (E)xtract, (R)oot, e(X)it\t\t: ");
-        menuOption = (keyboard.next()).toUpperCase();
-
-        // checks input
-        while (menuOption.charAt(0) != 'P' && menuOption.charAt(0) != 'E' && menuOption.charAt(0) != 'R' && menuOption.charAt(0) != 'X') {
-            System.out.print("Invalid menu option, please try again\t\t: ");
-            menuOption = (keyboard.next()).toUpperCase();
-            String famMember;
-
-            //print handling
-            if (menuOption.charAt(0) == 'P') {
-                for (i = 0; i < numFamMembers; i++){
-                    System.out.println(famNames[i]);
-                    System.out.print("\tUppers: ");
-                    for (j = 0; j < teeth[i][0].length; j++) {
-                        System.out.printf("%d:%c ", (j+1), teeth[i][0][j]);
-                    }
-                    System.out.println();
-                    System.out.print("\tLowers: ");
-                    for (j = 0; j < teeth[i][0].length; j++) {
-                        System.out.printf("%d:%c ", (j+1), teeth[i][1][j]);
-                    }
-                    System.out.println();
-                }
-                System.out.println();
-            }
-
-            //Extract handling
-            else if (menuOption.charAt(0) == 'E') {
-                System.out.print("Which family member \t\t: ");
-                famMember = keyboard.next();
-
-            }
-            //root handling
-            else if (menuOption.charAt(0) == 'R') {
-                System.out.printf("One root canal at\t%f", );
-            }
-            // exit handling
-            else if (menuOption.charAt(0) == 'X') {
-                System.out.println("Exiting the Floridian Tooth Records :-)");
-                break;
-            }
-
-
+                teeth [i][1] = tooths.toCharArray();
         }
 
-        //processing
+        menuOption = menuOpt();
+        while (menuOption != 'X') {
+            switch (menuOption) {
+                case 'P':
+                    printRecords(teeth, famNames, numFamMembers);
+                    break;
+                case 'E':
+                    extract(famNames, teeth);
+                    break;
+                case 'R':
+                    roots(numFamMembers, teeth);
+                    break;
+                case 'X':
+                    break;
+            }
+        }
+        System.out.println("Exiting the Floridian Tooth Records :-)");
 
-        //output
-    }//end of main method
+    }//End of main method
 
+
+    public static void printRecords(char[][][] teeth, String[] famNames, int numFamMembers) {
+
+
+        for (int famIndex = 0; famIndex < numFamMembers; famIndex++){
+            System.out.println(famNames[famIndex]);
+            System.out.print("\tUppers: ");
+            for (int toothIndex = 0; toothIndex < teeth[famIndex][0].length; toothIndex++) {
+                System.out.printf("%d:%c ", (toothIndex+1), teeth[famIndex][0][toothIndex]);
+            }
+            System.out.println();
+            System.out.print("\tLowers: ");
+            for (int toothIndex = 0; toothIndex < teeth[famIndex][0].length; toothIndex++) {
+                System.out.printf("%d:%c ", (toothIndex+1), teeth[famIndex][1][toothIndex]);
+            }
+            System.out.println();
+        }
+
+
+    }
+    private static char menuOpt() {
+        char choice;
+        System.out.print("(P)rint, (E)xtract, (R)oot, e(X)it :");
+        choice = keyboard.next().charAt(0);
+        choice = Character.toUpperCase(choice);
+        while (!(choice == 'P' || choice == 'E' || choice == 'R' || choice == 'X')) {
+            System.out.print("Invalid menu option, try again : ");
+            choice = keyboard.next().charAt(0);
+            choice = Character.toUpperCase(choice);
+        }
+        return choice;
+    }
+
+    private static void Print(String[] famNames, char[][][] teeth) {
+        for (String ignored : famNames)
+            System.out.println();
+        for (int info = 0; info < famNames.length; info++) {
+            System.out.println(famNames[info]);
+            System.out.print(" Uppers: ");
+            for (int memb = 0; memb < teeth[info][0].length; memb++)
+                System.out.print((memb + 1) + ":" + teeth[info][0][memb] + " ");
+            System.out.print(" Lowers: ");
+            for (int memb = 0; memb < teeth[info][1].length; memb ++)
+                System.out.print((memb + 1) + ":" + teeth[info][1][memb] + " ");
+            System.out.println();
+        }
+
+    }
+
+    public static int famMember(String[] names, String name){
+
+        for (int info = 0; info < names.length; info++) {
+            if (!names[info].equals(name)) continue;
+            return info;
+        }
+        return -1;
+    }
+
+    private static void extract(String[] famNames, char[][][] teeth) {
+        String name;
+        char row;
+        System.out.print("Which family member : ");
+        name = keyboard.next();
+        int fam = famMember(famNames, name);
+        while (fam < 0) {
+            System.out.print("Invalid family member, try again : ");
+            name = keyboard.next();
+            fam = famMember(famNames, name);
+        }
+        System.out.print("Which tooth layer (U)pper or (L)ower : ");
+        row = keyboard.next().charAt(0);
+        row = Character.toUpperCase(row);
+        while (!(row == 'U' || row == 'L')) {
+            System.out.print("Invalid layer, try again : ");
+            row = keyboard.next().charAt(0);
+            row = Character.toUpperCase(row);
+        }
+        int UppersOrLowers;
+        UppersOrLowers = 1;
+        System.out.print("Which tooth number : ");
+        int toothNumber = keyboard.nextInt();
+        while (true) {
+            if (toothNumber < 0 || toothNumber > MAX_TEETH) {
+                System.out.print("Invalid tooth number, try again : ");
+                toothNumber = keyboard.nextInt();
+                continue;
+            }
+            if (teeth[fam][UppersOrLowers][toothNumber - 1] == 'M') {
+                System.out.print("Missing tooth, try again : ");
+                toothNumber = keyboard.nextInt();
+                continue;
+            }
+            teeth[fam][UppersOrLowers][toothNumber - 1] = 'M';
+            break;
+        }
+
+
+    }
+    private static void roots(int numFamMembers, char [][][] teeth) {
+        double a,b,c,root1, root2, discriminant;
+        double iSum = 0.0;
+        double bSum = 0.0;
+        double mSum = 0.0;
+
+        for (int famIndex = 0; famIndex < numFamMembers; famIndex++) {
+            for (int rowIndex =0; rowIndex < MAX_TEETH;rowIndex ++){
+                for (int toothIndex = 0; toothIndex < teeth[rowIndex][famIndex].length; toothIndex++) {
+                    switch (teeth[famIndex][rowIndex][toothIndex]){
+                        case 'I':
+                            iSum += 1;
+                            break;
+                        case 'B':
+                            bSum += 1;
+                            break;
+                        case 'M':
+                            mSum += 1;
+                            break;
+                        default:
+                            System.out.print("Error in reading teeth information                    :");
+                    }
+                }
+            }
+        }
+        // Ix2+Bx-M
+        a = iSum;
+        b = bSum;
+        c = -1*mSum;
+        discriminant = b * b - 4.0 * a * c;
+        if (discriminant == 0.0) {
+            root1 = -b / (2.0 * a);
+            System.out.printf("One root canal at %6.2f\n", root1);
+            System.out.println();
+        }else if (discriminant> 0.0) {
+            root1 = (-b + Math.pow(discriminant, 0.5)) / (2.0 * a);
+            root2 = (-b - Math.pow(discriminant, 0.5)) / (2.0 * a);
+            System.out.printf("One root canal at %6.2f\nAnother root canal at %6.2f", root1, root2);
+            System.out.println();
+        }else {
+            System.out.println("No real roots");
+        }
+    }
 }// end of class
 /*
 1. Input
